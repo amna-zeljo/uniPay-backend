66d3666d1fbb58ff0f09428e58402f57a0ff7781
Amna <amna.zeljo@stu.ssst.edu.ba>
2025-05-21 22:37:35 +0200

    added DTO classes for home pages feature

---
+++ backend/src/main/java/com/unipay/backend/dto/AnnouncementDTO.java
@@ -0,0 +1,74 @@
+package com.unipay.backend.dto;
+
+import java.time.LocalDateTime;
+
+public class AnnouncementDTO {
+    private Integer id;
+    private String title;
+    private String content;
+    private String targetAudience;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private String creatorName;
+    
+    
+    public AnnouncementDTO() {
+    }
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTargetAudience() {
+        return targetAudience;
+    }
+
+    public void setTargetAudience(String targetAudience) {
+        this.targetAudience = targetAudience;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+}
