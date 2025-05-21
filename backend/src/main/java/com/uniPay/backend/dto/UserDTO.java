50336be1f4d5d6e0c237218cc82500288cfdcf9d
denialic <deni.alic@stu.ssst.edu.ba>
2025-05-21 17:44:39 +0200

    Created login method with database

---
+++ backend/src/main/java/com/uniPay/backend/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package com.uniPay.backend.dto;
+
+import com.uniPay.backend.entity.User;
+import jakarta.persistence.Column;
+
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
