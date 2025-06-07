affc84a9ebfa5086571b62b4079113c4c7312fde
Amna <146991640+amna-zeljo@users.noreply.github.com>
2025-06-07 19:38:51 +0200

    Implemented Login
    - Implemented the login feature. After logging in the userId is saved to the localStorage. This userId is later used to fetch the user data instead of the hardcoded id.
    - Fixed relations between user, customer, staff, wallet

--- backend/src/main/java/com/uniPay/backend/service/UserService.java
+++ backend/src/main/java/com/uniPay/backend/service/UserService.java
@@ -1,8 +1,13 @@
 package com.unipay.backend.service;
 
 import com.unipay.backend.dto.UserDTO;
+import com.unipay.backend.entity.Customer;
+import com.unipay.backend.entity.Staff;
 import com.unipay.backend.entity.User;
+import com.unipay.backend.entity.Wallet;
 import com.unipay.backend.model.MockUser;
+import com.unipay.backend.repository.CustomerRepository;
+import com.unipay.backend.repository.StaffRepository;
 import com.unipay.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,7 +42,37 @@ public class UserService {
         user.setPassword(userDTO.getPassword());
         user.setUsername(userDTO.getUsername());
         user.setRole(userDTO.getRole());
-        userRepository.save(user);
+        List<String> splitUsername = Arrays.stream(userDTO.getUsername().split(" ")).toList();
+        if("STAFF".equals(userDTO.getRole())){
+            Staff staff = new Staff();
+            staff.setPosition("Assistant");
+            staff.setUser(user);
+            staff.setFirstName(splitUsername.get(0));
+            if(splitUsername.size() > 1){
+                staff.setLastName(splitUsername.get(1));
+            }
+            staff.setFirstName(userDTO.getUsername().split(" ")[0]);
+            user.setStaff(staff);
+        }else {
+            Customer customer = new Customer();
+            customer.setFirstName(splitUsername.get(0));
+            if(splitUsername.size() > 1){
+                customer.setLastName(splitUsername.get(1));
+            }
+            customer.setStudentId(userDTO.getUsername());
+            customer.setUser(user);
+            user.setCustomer(customer);
+
+            Wallet wallet = new Wallet();
+            wallet.setUser(user);
+            double randomNumber = (double) Math.round((Math.random() * 50 + 1) * 100) / 100;
+            wallet.setBalance( randomNumber);
+            randomNumber = (double) Math.round((Math.random() * 50 + 1) * 100) / 100;
+            wallet.setPoints((int) randomNumber);
+            user.setWallet(wallet);
+        }
+        user = userRepository.save(user);
+
         return new UserDTO(user);
 
     }
